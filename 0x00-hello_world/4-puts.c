e16d9929a999536b813813b875aca7c7e03644cf
MisiCod <manuelmisiko2@gmail.com>
2022-07-01 00:41:20 +0300

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - ENtry point
+ *
+ * Description: This is a longer description.
+ * Calling the function puts
+ * Return: End the program in main
+ */
+int main(void)
+{
+puts();
+return (0);
+}
+/**
+ * puts - Prints on the current line
+ *
+ * Description: This is a longer description.
+ * Printing a line without using the printf command
+ * in C
+ * Return: The printed message
+ */
+int puts(void)
+{
+print ("\"Programming is like building a multilingual puzzle\n");
+}
