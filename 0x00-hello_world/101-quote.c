115d095ac47e66dedcc5e78062376a846858599e
MisiCod <manuelmisiko2@gmail.com>
2022-07-01 03:36:21 +0300

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Write out the error message
+ *
+ * Return: Return one
+ */
+int main(void)
+{
+fwrite(stderr, "and that piece of art is useful\"");
+fwrite(stderr, " - Dora Korpar, 2015-10-19\n");
+return (1);
+}
