199e8a28629cdbd6265adfeffdbc36c7e7a00dce
MisiCod <manuelmisiko2@gmail.com>
2022-07-01 18:18:11 +0300

    Positive anything is better than negative nothing

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - Generate a random number and tell if its -tive,0 or +tive
+ *
+ * Description: srand\() uses its argument seed as a seed for a new
+ * sequence of pseudo-random numbers to be returned by subsequent
+ * calls to rand\().
+ * The constant RAND_MAX is the maximum value that can be returned
+ * by the rand function. RAND_MAX is defined as the value 0x7fff.
+ * Return: Exit program.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
