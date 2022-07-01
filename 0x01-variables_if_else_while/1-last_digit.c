f61e904ae0cd8d84585065965a3069f9ace36bb9
MisiCod <manuelmisiko2@gmail.com>
2022-07-01 23:51:43 +0300

    The last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - Generate a random number and tell if its -tive,0 or +tive
+ *
+ * Description: srand\() uses its argument seed as a seed for a new
+ * sequence of pseudo-random numbers to be returned by subsequent
+ * calls to rand\().
+ * The constant RAND_MAX is the maximum value that can be returned
+ * by the rand function. RAND_MAX is defined as the value 0x7fff.
+ * Return: Exit program.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 5)
+	{
+		printf("Last digit of n is %d and is greater than 5\n", n);
+	}
+	else if (n < 6 & n > 0)
+	{
+		printf("Last digit of n is %d and is less than 6 not 0 \n", n);
+	}
+	else
+	{
+		printf("Last digit of n is %d and is zero\n", n);
+	}
+	return (0);
+}
