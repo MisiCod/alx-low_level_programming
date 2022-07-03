9665c75013daa6fa3bd1453433b1078609ffc893
MisiCod <manuelmisiko2@gmail.com>
2022-07-04 00:27:27 +0300

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,7 +2,7 @@
 /**
  * main - Print all single digit numbers of base 10 starting from 0
  *
- * Description: Write a program that prints all single digit 
+ * Description: Write a program that prints all single digit
  * numbers of base 10 starting from 0, followed by a new line.
  * Return: Exit program
  */
@@ -11,7 +11,7 @@ int main(void)
 {
 	char ch;
 
-	for (ch = '1' ; ch <= '9' ; ch++)
+	for (ch = '0' ; ch <= '9' ; ch++)
 	{
 		putchar (ch);
 	}
