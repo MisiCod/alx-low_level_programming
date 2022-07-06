7eefbab009c24743ca1f04f3a5f82afd2868744e
MisiCod <manuelmisiko2@gmail.com>
2022-07-06 04:24:17 +0300

    _putchar

---
+++ 0x02-functions_nested_loops/1.alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - Print alphabet in lowercase
+ *
+ * Description: Write a program that prints the
+ * alphabet in lowercase, followed by a new line.
+ * Return: Exit program
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+/**
+ * print_alphabet - Function to print alphabet
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar (ch);
+	}
+	putchar('\n');
+}
