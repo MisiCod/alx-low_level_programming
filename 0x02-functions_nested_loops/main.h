689e5a065471036b9875e0cdef3ce1448ccd85fe
MisiCod <manuelmisiko2@gmail.com>
2022-07-06 05:52:15 +0300

    10 x alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 void print_alphabet(void);
+void print_alphabet_x10(void);
 /**
  * print_alphabet - Function to print alphabet
  */
@@ -13,3 +14,22 @@ void print_alphabet(void)
 	}
 	putchar('\n');
 }
+/**
+ * print_alphabet 10 times - Function to print alphabet
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i = 0;
+	while (i<9)
+	{
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+		{
+			putchar (ch);
+		}
+		i ++;
+	}
+	putchar('\n');
+	return (0);
+}
+
