7d8193c288f738802ef97dc89baf9a05e7a9e2fd
MisiCod <manuelmisiko2@gmail.com>
2022-07-06 05:57:44 +0300

    10 x alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -21,7 +21,7 @@ void print_alphabet_x10(void)
 {
 	char ch;
 	int i = 0;
-	while (i<9)
+	while (i < 9)
 	{
 		for (ch = 'a' ; ch <= 'z' ; ch++)
 		{
@@ -30,6 +30,5 @@ void print_alphabet_x10(void)
 		i ++;
 	}
 	putchar('\n');
-	return (0);
 }
 
