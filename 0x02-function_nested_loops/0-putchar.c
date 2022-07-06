1568ecdb8625b4376738555215b644e3b29c089a
MisiCod <manuelmisiko2@gmail.com>
2022-07-06 03:09:54 +0300

    _putchar

---
+++ 0x02-function_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A program that prints _putchar, followed
+ * by a new line.
+ *
+ * Return: Exit program.
+ */
+
+int main(void)
+{
+	char output[] = "_putchar";
+		for (int i = 0; i <= 7; i++)
+		{
+			putchar (output[i]);
+		}
+	return (0);
+}
