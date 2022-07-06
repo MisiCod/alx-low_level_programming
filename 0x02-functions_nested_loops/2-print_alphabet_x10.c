2ba0e8013607cbc2260d870509db863cfffee595
MisiCod <manuelmisiko2@gmail.com>
2022-07-06 06:05:14 +0300

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,15 @@ int alphabet_ten_times(void)
 {
 	char ch;
 	int i = 0;
-	while (i<9)
+	
+	while (i < 9)
 	{
 		for (ch = 'a' ; ch <= 'z' ; ch++)
 		{
 			putchar (ch);
 		}
+		putchar('\n');
 		i ++;
 	}
-	putchar('\n');
 	return (0);
 }
