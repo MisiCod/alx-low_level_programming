d5fc6ebf6a52ef41b127de707be2c849183aeb6d
MisiCod <manuelmisiko2@gmail.com>
2022-07-08 01:47:44 +0300

    isupper

---
+++ 0x02-functions_nested_loops/0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * _isupper - checks if the character is uppercase
+ * @c:  passed argument
+ * Return: 1 if uppercase 0 if not
+ */
+int _isupper(int c)
+{
+	int confirm;
+
+	confirm = (isupper(c));
+	return (confirm);
+}
