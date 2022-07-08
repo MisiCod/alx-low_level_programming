d3675ddd7ed5918abdde815c23f61457bda50120
MisiCod <manuelmisiko2@gmail.com>
2022-07-08 06:05:41 +0300

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: void.
+ */
+int main (void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if ((i % 3) == 0)
+		{
+			printf ("Fizz ");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf ("Buzz ");
+	        }
+		else if (((i % 3) == 0) && ((i % 5) == 0))
+		{
+			printf ("Fizz Buzz ");
+		}
+		else
+		{
+			printf ("%d ", i);
+		}
+		i++;
+	}
+	printf ("\n");
+	return (0);
+}
