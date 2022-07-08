b73a986bd1af045482a123c0b4f07e5db05e0b06
MisiCod <manuelmisiko2@gmail.com>
2022-07-08 05:32:48 +0300

    I feel like I am diagonally parked in a parallel universe

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check for a digit
+ * @n : number of \\ to be printed
+ *
+ * Return:void
+ */
+
+void print_diagonal(int n)
+{
+	int times = 0;
+
+	while (times < n && n > 0)
+	{
+		_putchar('\\');
+		i++;
+	}
+	_putchar('\n');
+
+}
