c232fef5d3ccdb36bb509440eef58356f4942c9d
MisiCod <manuelmisiko2@gmail.com>
2022-07-09 20:07:16 +0300

    Battery st.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: passed character
+ *
+ * Return: program exit.
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
