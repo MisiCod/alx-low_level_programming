1b355a0a7c22f91281277f4c643113c3748acf01
MisiCod <manuelmisiko2@gmail.com>
2022-07-09 19:44:52 +0300

    Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,14 +2,11 @@
 /**
  * reset_to_98 - takes a pointer to an int as parameter
  * and updates the value it points to to 98
+ * @n: passed pointer address
  *
  * Return: void
  */
 void reset_to_98(int *n)
 {
-	int *p;
-	*p = &n;
-
-	*p = 98;
-	return(void);
+	*n = 98;
 }
