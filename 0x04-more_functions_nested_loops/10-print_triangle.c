5e69dd4c3a08d4951008840ff4f2ab65f5a4b53d
MisiCod <manuelmisiko2@gmail.com>
2022-07-09 04:42:40 +0300

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,20 +8,27 @@ void print_triangle(int size)
 {
 	int lines = 0;
 
-	while (lines < size)
+	if (size <= 0)
 	{
-		int rows;
-		int col = 0;
-
-		for (rows = lines; rows < size - 1; rows++)
-		{
-			_putchar(' ');
-		}
-		for (rows = lines; rows >= col; col++)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
-		lines++;
+	}
+	else
+	{
+		while (lines < size)
+		{
+			int rows;
+			int col = 0;
+
+			for (rows = lines; rows < size - 1; rows++)
+			{
+				_putchar(' ');
+			}
+			for (rows = lines; rows >= col; col++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+			lines++;
+		}
 	}
 }
